20a2591722c38f35e2e8e3f444ee36beb0a5ff77
Majiir Paktu <majiir@nabaal.net>
2014-06-24 00:42:03 -0400

    Basic random browsing with database

---
+++ routes.js
@@ -0,0 +1,30 @@
+var Image = require('./models/image.js');
+
+module.exports = exports = function (app, images) {
+
+	app.get('/', function (req, res) {
+		res.render('index');
+	});
+
+	app.get('/random', function (req, res) {
+		var id = images[Math.floor(Math.random() * images.length)];
+		res.redirect('/v/' + image);
+	});
+
+	app.get(/^\/v\/(\w+)$/, function (req, res) {
+		var id = req.params[0];
+		Image.findOne({ id: id }, function (err, image) {
+			if (err) { res.send(500); return; }
+			if (!image) { res.send(404); return; }
+			res.format({
+				html: function () {
+					res.render('viewer', image);
+				},
+				json: function () {
+					res.json(image);
+				},
+			});
+		});
+	});
+
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+var cons = require('consolidate');
+var express = require('express');
+var mongoose = require('mongoose');
+var Image = require('./models/image.js');
+
+mongoose.connect('mongodb://localhost/majiirnet');
+
+var app = express();
+
+require('dustjs-helpers');
+app.engine('dust', cons.dust);
+
+app.enable('trust proxy');
+app.disable('etag');
+app.disable('x-powered-by');
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/templates');
+
+var images = [];
+console.log('Querying image index...');
+Image.find({}, { _id: 0, id: 1 }).stream().on('data', function (image) {
+	images.push(image.id);
+}).on('close', function () {
+	console.log('Displaying ' + images.length + ' images.');
+});
+
+require('./routes.js')(app, images);
+
+app.listen(3000);
